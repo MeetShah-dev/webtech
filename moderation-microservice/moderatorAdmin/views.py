0cb1d2d118fb449096ce9964676e244274386bb9
Irmak Tural <121566642+sololospeces1@users.noreply.github.com>
2024-04-15 01:00:02 +0100

    added category features

--- moderation-microservice/moderatorAdmin/views.py
+++ moderation-microservice/moderatorAdmin/views.py
@@ -5,26 +5,38 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from moderatorAdmin.models import Blog, User, Magazine, Category, Comment, Role
-from moderatorAdmin.serializer import  CategorySerializerA,ManyCategorySerializer, CommentSerializer, RoleSerializer,UserRoleSerializer,BlogSerializer, UserSerializer, MagazineSerializer,CategorySerializer,UserSerializerB,CategorySerializer,FeedbackSerializerUp
+from moderatorAdmin.serializer import  CategoryBlogUpSerializer,BlogCategorySerializer, CategorySerializerA,ManyCategorySerializer, CommentSerializer, RoleSerializer,UserRoleSerializer,BlogSerializer, UserSerializer, MagazineSerializer,CategorySerializer,UserSerializerB,CategorySerializer,FeedbackSerializerUp
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.utils import timezone
 from rest_framework.renderers import JSONRenderer
+import json,urllib
+import requests
+import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
     
+api_endpoint_url = os.environ.get('API_ENDPOINT_URL')
 
 #----------------------------------------#Post Feedback oldu ama notificationı düşün ------------------------------------------
 @csrf_exempt
 @api_view(['POST'])
 
 def postFeedback(request):
-    
+    pk = request.data.get('blog')  # Get pk from request data
+    content = request.data.get('content')  # Get pk from request data
     # List all code snippets, or create a new snippet.
-  
+    instance = Blog.objects.get(pk=pk)
+    
+    print("*****************", instance.user.pk)#user id sini aldı 
     if request.method == 'POST':
         serializer = FeedbackSerializerUp(data=request.data)
+      
         if serializer.is_valid():
             serializer.save()
+            notifyUser(api_endpoint_url, pk, 1, receiver=2, type="feedback")
             return Response(serializer.data, status=status.HTTP_201_CREATED)
             snippet_listPOST(request)
            
@@ -45,6 +57,8 @@ def addComment(request):
           
            
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
  #----------------------------------------#GET COMMENTS AVERALL ------------------------------------------
 
 @csrf_exempt
@@ -132,6 +146,38 @@ def getAllUsers(request):
         category = User.objects.all()
         serializer = UserSerializer(category, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+#------------ADD CATEGORY TO BLOG--------------------------------
+
+@csrf_exempt
+@api_view(['PUT'])
+def addCategoryToBlog(request):
+        try:
+            blog = Blog.objects.get(id=request.data.get('id'))
+            #category = Category.objects.get(id=request.data.get('category_id'))
+            #print("XXXXXX", category.name)
+        except Blog.DoesNotExist:
+            return Response({"error": "Blog does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        except Category.DoesNotExist:
+            return Response({"error": "Category does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        if isinstance(request.data.get('category_id'), int): 
+            category = Category.objects.get(id=request.data.get('category_id'))
+            blog.categories.add(category)
+        elif isinstance(request.data.get('category_id'), list):  # Check if it's a list of IDs
+            for category_id in request.data.get('category_id'):
+                try:
+                    category = Category.objects.get(id=category_id)
+                    blog.categories.add(category)
+               
+                
+                except Category.DoesNotExist:
+                    return Response({"error": f"Category with ID {category_id} does not exist"}, status=status.HTTP_404_NOT_FOUND)
+         
+        #print("xxxx",blog.categ)
+        #blog.categories.add(category)
+        #print("yyyyy", blog)
+        #print("xxxxxx", blog.categories)
+        serializer = BlogCategorySerializer(blog)
+        return Response(serializer.data)
 
 #-------------------------------DELETE CATEGORY-------------------"
     
@@ -257,14 +303,18 @@ def approvePost(request):
 
     try:
         instance = Blog.objects.get(pk=pk)
+
+        print("*****************", instance.user.pk)#user id sini aldı 
         instance.is_approved=True
         instance.is_rejected=False
     except Blog.DoesNotExist:
         return Response({"error": "Instance with the provided primary key does not exist."}, status=status.HTTP_404_NOT_FOUND)
 
     serializer =BlogSerializer(instance, data=request.data)
+    
     if serializer.is_valid():
         serializer.save()
+        notifyUser(api_endpoint_url, pk, 1, receiver=2, type="blog-approval")
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
@@ -296,7 +346,31 @@ def deletePostX(requests):
     except requests.exceptions.RequestException as e:
         # Handle request exceptions
         return Response({'error': str(e)}, status=500)
+    
+def call_api_with_json_payload(blogValue):
+    url = 'http://54.82.93.84:8000/api/delete-blog/'
+    payload = {{"user": "1", "blog": blogValue} }  # JSON payload to be sent
 
+    # Convert payload dictionary to JSON string
+    payload_json = json.dumps(payload)
+
+    # Encode JSON payload as URL-safe string
+    payload_encoded = urllib.parse.quote(payload_json)
+
+    # Construct URL with encoded JSON payload
+    url_with_params = f"{url}?data={payload_encoded}"
+
+    try:
+        # Make GET request
+        request = HttpRequest()
+        response = request.GET(url_with_params)
+
+        # Assuming response is JSON
+        data = json.loads(response.content.decode('utf-8'))
+        # Process the response data as needed
+        print(data)
+    except Exception as e:
+        print("Error:", e)
 
 @api_view(['PUT'])
 def rejectPost(request):
@@ -306,16 +380,17 @@ def rejectPost(request):
 
     try:
         instance = Blog.objects.get(pk=pk)
+        print("*****************", instance.user.pk)#user id sini aldı 
  
-        if(instance.rejection_number>1):
-           
-            print("")#call Rayan's API 
-            
-        else : 
-            instance.is_approved=False
-            instance.is_rejected=True
-            instance.ready=False
-            instance.rejection_number= instance.rejection_number+1
+      
+        user_id=1
+        
+        
+        instance.is_approved=False
+        instance.is_rejected=True
+        instance.ready=False
+        instance.rejection_number= instance.rejection_number+1
+     
         
     except Blog.DoesNotExist:
         return Response({"error": "DELETED"})
@@ -324,6 +399,7 @@ def rejectPost(request):
     if serializer.is_valid():
         if(instance.rejection_number<=1):
             serializer.save()
+            notifyUser(api_endpoint_url, pk, user_id, receiver=2,type="blog-rejection")
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -365,9 +441,97 @@ def deleteUser(request):
     return Response(serializer.data, status=204)
 
 
+def notifyUser(url, blog, sender,receiver, type):
+
+    try:
+        # URL of the API you want to send the POST request to
+        api_url = url
+
+        # Data to be sent in the POST request (in JSON format)
+        post_data = {'blog': blog, 'sender': sender,'receiver': receiver, 'type': type }
+
+        # Make a POST request to the API
+        response = requests.post(api_url, json=post_data)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200 or response.status_code == 201:
+            # Parse the JSON response
+            data = response.json()
+
+            # Process the data as needed
+            # For example, you can return it directly as JSON
+            return Response(data)
+        else:
+            # If the request was not successful, return an error response
+            return Response({'error': f'Failed to send POST request to API. Status code: {response.status_code}'}, status=response.status_code)
+    except requests.RequestException as e:
+        # If an error occurs during the request, return an error response
+        return Response({'error': f'Failed to send POST request to API. Error: {str(e)}'}, status=500)
+
+
+
+
+
+
+
+
+def notifyTest(url, blog, content):
+
+    try:
+        # URL of the API you want to send the POST request to
+        api_url = url
+
+        # Data to be sent in the POST request (in JSON format)
+        post_data = {'blog': blog, 'content': content}
+
+        # Make a POST request to the API
+        response = requests.post(api_url, json=post_data)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200 or response.status_code == 201:
+            # Parse the JSON response
+            data = response.json()
+
+            # Process the data as needed
+            # For example, you can return it directly as JSON
+            return Response(data)
+        else:
+            # If the request was not successful, return an error response
+            return Response({'error': f'Failed to send POST request to API. Status code: {response.status_code}'}, status=response.status_code)
+    except requests.RequestException as e:
+        # If an error occurs during the request, return an error response
+        return Response({'error': f'Failed to send POST request to API. Error: {str(e)}'}, status=500)
+
+
 
 @csrf_exempt
 @api_view(['GET'])
 def test_api(request):
+    print("HELLO TEST")
+    return Response("successsssssssssssssssssss")
 
-    return JsonResponse("aaaaaa====test", safe=False)
+   
+
+    
+@csrf_exempt
+def my_view(request):
+    # URL of the API you want to call
+    api_url = 'https://api.example.com/data/'
+    try:
+        # Make a GET request to the API
+        response = requests.get(api_url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+
+            # Process the data as needed
+            # For example, you can return it directly as JSON
+            return JsonResponse(data)
+        else:
+            # If the request was not successful, return an error response
+            return JsonResponse({'error': f'Failed to fetch data from API. Status code: {response.status_code}'}, status=response.status_code)
+    except requests.exceptions.RequestException as e:
+        # If an error occurs during the request, return an error response
+        return JsonResponse({'error': f'Failed to fetch data from API. Error: {str(e)}'}, status=500)
