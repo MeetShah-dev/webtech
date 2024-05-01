56104bb49986fba4d156f5fb109597db0ceb147d
Rayan Meraghni <meraghnir93@gmail.com>
2024-05-01 16:39:01 +0100

    frontend

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,96 @@
+<template>
+    <nav>
+        <div class="logo">
+            <img
+                alt="Surrey Horizon logo"
+                class="logo"
+                src="@/assets/Logo.png"
+            />
+            <h1>SURREY HORIZON</h1>
+        </div>
+        <v-toolbar flat dark app color="#114C6E">
+            <v-tabs>
+                <v-tab exact to="/" active>Home</v-tab>
+                <v-tab exact to="/dashboard">Dashboard</v-tab>
+                <v-tab exact to="/editing">Editing</v-tab>
+            </v-tabs>
+            <v-spacer></v-spacer>
+            <v-menu open-on-hover bottom offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on">
+                        <v-btn icon>
+                            <v-icon>mdi-bell</v-icon>
+                        </v-btn>
+                    </v-btn>
+                </template>
+
+                <v-list v-bind:width="370">
+                    <v-list-item v-for="(notif, index) in notifs" :key="index">
+                        <v-list-item-title>{{ notif.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+            <v-menu bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on">
+                        <v-icon>mdi-menu</v-icon>
+                    </v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item v-for="(menu, index) in menus" :key="index">
+                        <v-list-item-title>{{ menu.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+
+            <v-btn>
+                <span>Sign Out</span>
+                <v-icon right>mdi-exit-to-app</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+// import UserRegistration from '../components/UserRegistration.vue';
+export default {
+    name: 'nav-bar',
+    data() {
+        return {
+            links: [
+                { tab: 'home', text: 'Home', route: '/' },
+                { tab: 'dashboard', text: 'Dashboard', route: '/dashboard' },
+                { tab: 'editing', text: 'Editing', route: '/editing' },
+                { tab: 'author', text: 'Authors', route: '/author-page' },
+            ],
+            menus: [
+                { title: 'Author page' },
+                { title: 'Blog list' },
+                { title: 'Update profile', components: 'UserRegistration' },
+            ],
+            notifs: [{ title: 'Notification 1' }, { title: 'Notification 2' }],
+        };
+    },
+    components: {
+        // UserRegistration,
+    },
+};
+</script>
+
+<style scoped>
+.logo {
+    display: flex;
+    flex-direction: row;
+}
+.logo h1 {
+    height: 120px;
+    font-size: 30pt;
+    font-stretch: ultra-expanded;
+    padding-top: 23px;
+}
+
+.logo img {
+    height: 120px;
+}
+</style>
---
+++ frontend/src/views/MagazineView.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="main-container">
+        <div class="side-container">
+            <v-list>
+                <v-list-tile v-for="link in links" :key="link.text">
+                    <router-link :to="link.route" tag="div">
+                        <v-list-tile-content>
+                            <v-list-tile-title>{{
+                                link.text
+                            }}</v-list-tile-title>
+                        </v-list-tile-content>
+                    </router-link>
+                </v-list-tile>
+            </v-list>
+        </div>
+        <div class="content-container">
+            <div class="article-view">
+                <router-view></router-view>
+            </div>
+            <div class="like-section">
+                <h3>Like Section</h3>
+                <v-btn @click="handleLike">Like</v-btn>
+            </div>
+
+            <!-- Comment Section -->
+            <div class="comment-section">
+                <h3>Comment Section</h3>
+                <!-- Your comment component can go here -->
+                <textarea
+                    v-model="comment"
+                    placeholder="Enter your comment"
+                ></textarea>
+                <v-btn @click="postComment">Post Comment</v-btn>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                { text: 'Article Title One', route: '/article/1' },
+                { text: 'Article Title Two', route: '/article/2' },
+                { text: 'Article Title Three', route: '/article/3' },
+                { text: 'Article Title Four', route: '/article/4' },
+                { text: 'Article Title Five', route: '/article/5' },
+                { text: 'Article Title Six', route: '/article/6' },
+            ],
+            comment: '', // Store user's comment
+        };
+    },
+    methods: {
+        handleLike() {
+            alert('Liked!');
+        },
+        postComment() {
+            if (this.comment.trim() !== '') {
+                console.log('Posting comment:', this.comment);
+                this.comment = '';
+            }
+        },
+    },
+};
+</script>
+
+<style scoped>
+.main-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 1000px;
+}
+
+.side-container {
+    height: 1000px;
+    width: 20%;
+    padding: 10px;
+    background-color: #c1e2f4;
+}
+
+.content-container {
+    height: 1000px;
+    width: 70%;
+    padding: 10px;
+    background-color: #114c6e;
+    color: #fff;
+}
+
+.like-section,
+.comment-section {
+    margin-top: 20px;
+    padding: 10px;
+    border-top: 1px solid #fff;
+}
+
+textarea {
+    width: 100%;
+    height: 100px;
+    margin-bottom: 10px;
+}
+</style>
